11ff6f5ccaea2851509035ec284c1221832334ef
izackcosta <izackcosta97@gmail.com>
2025-08-20 22:07:31 -0300

    created movie card component

---
+++ src/components/MovieCard.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+type MovieCardProps = {
+
+};
+
+const MovieCard: React.FC<MovieCardProps> = ({
+
+}) => {
+    return (
+        <div className='movie-card' style={{ border: '1px solid #ddd', borderRadius: 8, padding: 16, maxWidth: 300 }}>
+            
+        </div>
+    );
+};
+
+export default MovieCard;
