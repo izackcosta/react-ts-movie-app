539af7805e7d8f00505ae94e127f13e2000ca8f6
izackcosta <izackcosta97@gmail.com>
2025-08-22 13:12:03 -0300

    added favorites page and routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
+import { Routes, Route } from 'react-router-dom'
 import './App.css'
-import MovieCard from './components/MovieCard'
 import Home from './pages/Home'
+import Favorites from './pages/Favorites'
 
 function App() {
 
   return (
     <>
+      <main>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/favorites' element={<Favorites />} />
+        </Routes>
+      </main>
       <Home />
     </>
   )
---
+++ src/pages/Favorites.tsx
@@ -0,0 +1,12 @@
+import type { FC } from "react";
+
+
+const Favorites : FC = () => {
+    return (
+        <div className='favorites-empty'>
+            <h2>No favorite movies yet...</h2>
+        </div>
+    );
+}
+
+export default Favorites;
