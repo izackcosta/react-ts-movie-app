6eb03464e4e672893b511c5545053ab8704e9308
izackcosta <izackcosta97@gmail.com>
2025-08-22 13:27:46 -0300

    navbar added

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,21 @@ import { Routes, Route } from 'react-router-dom'
 import './App.css'
 import Home from './pages/Home'
 import Favorites from './pages/Favorites'
+import NavBar from './components/NavBar'
 
 function App() {
 
   return (
     <>
-      <main>
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/favorites' element={<Favorites />} />
-        </Routes>
-      </main>
+      <div>
+        <NavBar />
+        <main>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/favorites' element={<Favorites />} />
+          </Routes>
+        </main>
+      </div>
     </>
   )
 }
---
+++ src/components/NavBar.tsx
@@ -0,0 +1,18 @@
+import type { FC } from "react";
+import { Link } from "react-router-dom";
+
+const NavBar : FC = () => {
+    return (
+        <nav className='navbar'>
+            <div className='navbar-brand'>
+                <Link to='/'>Movie App</Link>
+            </div>
+            <div className='navbar-links'>
+                <Link to='/' className="nav-link">Home</Link>
+                <Link to='/favorites' className="nav-link">Favorites</Link>
+            </div>
+        </nav>
+    );
+}
+
+export default NavBar;
